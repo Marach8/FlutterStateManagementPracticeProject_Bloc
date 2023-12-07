70de809087873f242be0b18cbf1d9ab64ff68504
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2023-12-07 04:18:22 +0100

    Created a seperate file for the cubit example

--- bloc_practice_course/lib/main.dart
+++ bloc_practice_course/lib/main.dart
@@ -1,5 +1,4 @@
-import 'dart:math';
-import 'package:bloc/bloc.dart';
+import 'package:bloc_practice_course/cubit_example.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,62 +22,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Homepage extends StatefulWidget {
-  const Homepage({super.key});
-
-  @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-  late final NamesCubit namesCubit;
-
-  @override 
-  void initState(){super.initState; namesCubit = NamesCubit();}
-
-  @override 
-  void dispose(){namesCubit.close(); super.dispose();}
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Cubit Example'), centerTitle: true), 
-      body: StreamBuilder<String?> (
-        stream: namesCubit.stream,
-        builder: (context, snapshot){
-          final Widget button = TextButton(
-            onPressed: () => namesCubit.newCubitState(),
-            child: const Text('Pick a random name')
-          );
-          switch (snapshot.connectionState){
-            case ConnectionState.none:
-              return button;
-            case ConnectionState.waiting:
-              return button;
-            case ConnectionState.active:
-              return Column(children: [button, Text(snapshot.data ?? '')],);
-            case ConnectionState.done:
-              return const SizedBox();
-          }
-        }
-      )
-    );
-  }
-}
-
-
-
-
-
-
-const names = ['Emmanuel', 'David', 'Emeka', 'Daniel'];
-
-class NamesCubit extends Cubit<String?> {
-  NamesCubit(): super(null);
-
-  void newCubitState() => emit(names.marach());
-}
-
-extension Marach<T> on Iterable<T>{
-  T marach() => elementAt(Random().nextInt(length));
-}
